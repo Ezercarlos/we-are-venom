5bec470b0847612836dc77064f78f289b0c2a0d7
leef1 <86453556+leef1@users.noreply.github.com>
2021-07-01 23:10:04 +0800

    test 1
    no vars, no vals

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from calc import Calculator
+
+class Test(unittest.TestCase):
+    def test_contact(self):
+
+        self.assertEqual()
+
+
+if __name__ == "__main__":
+    unittest.main()
